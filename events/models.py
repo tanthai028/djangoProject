348bece0913fd9a58df609696b7c1114c0a0263d
Tan <35444054+tanthai028@users.noreply.github.com>
2021-07-31 13:03:31 -0400

    no module name 'models'

---
+++ events/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Event(models.Model):
+    day = models.DateField(u'Day of the event', help_text=u'Day of the event')
+    start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
+    end_time = models.TimeField(u'Final time', help_text=u'Final time')
+    notes = models.TextField(u'Textual Notes', help_text=u'Textual Notes', blank=True, null=True)
+
+    class Meta:
+        verbose_name = u'Scheduling'
+        verbose_name_plural = u'Scheduling'
+
+
